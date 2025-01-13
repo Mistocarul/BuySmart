16b5441aba2e32cda01e13b86ad31c1253e0ab5f
APK02 <126094928+APK02@users.noreply.github.com>
2025-01-13 23:38:08 +0200

    add some new queries

--- BuySmart/Application/DTOs/BusinessDto.cs
+++ BuySmart/Application/DTOs/BusinessDto.cs
@@ -2,15 +2,15 @@
 {
     public class BusinessDto
     {
-        
+        public Guid BusinessId { get; set; } = Guid.Empty;
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
 
         //User Business: Business (1:1)
-        public Guid UserBusinessId { get; set; }    //cel care vinde 
-        
-        
+        public Guid UserBusinessId { get; set; } = Guid.Empty;
+
+
     }
 }
--- BuySmart/Domain/Repositories/IBusinessRepository.cs
+++ BuySmart/Domain/Repositories/IBusinessRepository.cs
@@ -7,6 +7,7 @@ namespace Domain.Repositories
     {
         Task<IEnumerable<Business>> GetAllAsync();
         Task<Business> GetByIdAsync(Guid businessId);
+        Task<Business> GetByUserBusinessIdAsync(Guid userId);
         Task<Result<Guid>> AddAsync(Business business);
         Task<Result<object>> UpdateAsync(Business business);
         Task DeleteAsync(Guid businessId);
